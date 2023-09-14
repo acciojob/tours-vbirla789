fdb81382b43c4706520c2bf40a800f1e6069765d
VIIBII <birlavishal90@gmail.com>
2023-09-14 23:38:38 +0530

    submitting code-Thu Sep 14 2023 23:38:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-    return(
-      <main id="main">
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const removeTour = (id) => {
+    setTours(tours.filter((tour) => tour.id !== id));
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const fetchTours = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+
+      const toursFetched = await response.json();
+      setTours(toursFetched);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
       </main>
-    )
-}
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main id="main">
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main id="main">
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+
+// class App extends React.Component{
+
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       tours:[],
+//       loading: true
+//     }
+
+//     this.fetchTours = this.fetchTours.bind(this)
+//     this.removeTour = this.removeTour.bind(this)
+//   }
+
+//   removeTour(id){
+//     this.setState({tours:this.state.tours.filter((tour)=>tour.id!==id)})
+//   }
+//   // fetchTours = async () => {
+
+//   // }
+
+//   componentDidMount(){
+//     this.fetchTours()
+//   }
+//   async fetchTours (){
+//     try{
+//       this.setState({loading:true})
+//       const response = await fetch(url)
+
+//       const toursFetched = await response.json()
+//       console.log(toursFetched)
+//       this.setState({tours:toursFetched,loading:false})
+//     }catch(err){
+//       this.setState({loading:false})
+//       console.log(err)
+
+//     }
+//   }
+
+//   render(){
+
+//     if(this.state.loading){
+//       return(
+//         <main>
+//           <Loading />
+//         </main>
+//       )
+//     }
+
+//     if(this.state.tours.length === 0 ){
+//       return(
+//           <main>
+//             <div className="title">
+//               <h2>No tours left</h2>
+//               <button className="btn" onClick={this.fetchTours}>refresh</button>
+//             </div>
+//           </main>
+//       )
+//     }
+//     return(
+//       <main>
+//         <Tours tours={this.state.tours} removeTour={this.removeTour}/>
+//       </main>
+//     )
+//   }
+// }
+
 export default App;
---
+++ src/components/Tour.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function Tour({ id, image, info, name, price, removeTour, idx }) {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour" id={`tour-item-${id}`}>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">$ {price}</h4>
+        </div>
+        <p id={`tour-item-para-${id}`}>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)} id={`see-more-${id}`}>
+            {readMore ? "Show less" : "See more"}
+          </button>
+        </p>
+        <button
+          className="delete-btn"
+          onClick={() => console.log(id)}
+          id={`delete-btn-${id}`}
+        >
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+// class Tour extends React.Component {
+
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             readMore: false
+//         }
+//         this.toggleReadMore = this.toggleReadMore.bind(this)
+//     }
+
+//     toggleReadMore(){
+//         this.setState({readMore:!this.state.readMore})
+//     }
+
+//     render(){
+//         const {id,image,info,name,price,removeTour} = this.props
+//         return(
+//             <article className="single-tour">
+//                 <img src={image} alt={name} />
+//                 <footer>
+//                     <div className="tour-info">
+//                         <h4>{name}</h4>
+//                         <h4 className="tour-price">$ {price}</h4>
+//                     </div>
+//                     <p>
+//                         {this.state.readMore ? info : `${info.substring(0,200)}...`}
+//                         <button onClick={this.toggleReadMore}>{this.state.readMore ? 'show less' : 'read more'}</button>
+//                     </p>
+//                     <button className="delete-btn" onClick={()=>removeTour(id)}>not interested</button>
+//                 </footer>
+//             </article>
+//         )
+//     }
+// }
+export default Tour;
